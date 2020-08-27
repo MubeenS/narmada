7c44115b4821e8cc85e864a83170044f8607cd30
MubeenS <sk.md.mubeen@gmail.com>
2020-08-27 15:12:05 +0530

    Created DB schema

---
+++ esb_app/src/db_access/create_schema.sql
@@ -0,0 +1,54 @@
+.open test.db
+CREATE TABLE routes (
+    route_id INT PRIMARY KEY,
+    sender VARCHAR(45) NOT NULL UNIQUE,
+    destination VARCHAR(45) NOT NULL UNIQUE,
+    message_type VARCHAR(45) NOT NULL,
+    is_active BIT(1) NOT NULL
+);
+CREATE INDEX UK_sender_dest_msg_type 
+ON routes(sender,destination,message_type);
+
+CREATE TABLE transform_config (
+    id INT PRIMARY KEY,
+    route_id INT NOT NULL,
+    config_key VARCHAR(45) NOT NULL,
+    config_value TEXT,
+    FOREIGN KEY (route_id) REFERENCES routes
+	ON DELETE CASCADE
+);
+CREATE INDEX route_idx 
+ON transform_config(route_id);
+
+CREATE INDEX UK_route_config 
+ON transform_config(route_id,config_key,config_value);
+
+CREATE TABLE transport_config (
+    id INT PRIMARY KEY,
+    route_id INT,
+    config_key VARCHAR(45),
+    config_value TEXT,
+    FOREIGN KEY (route_id) REFERENCES routes
+	ON DELETE CASCADE
+);
+CREATE INDEX route_idx_tp 
+ON transport_config(route_id);
+
+CREATE INDEX UK_route_config_tp 
+ON transport_config(route_id,config_key,config_value);
+
+
+CREATE TABLE esb_request (
+    id INT PRIMARY KEY,
+    sender_id VARCHAR(45) NOT NULL,
+    dest_id VARCHAR(45) NOT NULL,
+    message_type VARCHAR(45) NOT NULL,
+    reference_id VARCHAR(45) NOT NULL,
+    message_id VARCHAR(45) NOT NULL,
+    received_on DATETIME NOT NULL,
+    data_location TEXT,
+    curr_status VARCHAR(20),
+    status_details TEXT
+)
+
+
