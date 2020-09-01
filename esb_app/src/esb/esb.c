2549c8af006983ca9676ff263ef96d3b3c4bbe80
MubeenS <sk.md.mubeen@gmail.com>
2020-09-01 19:51:23 +0530

    skeleton update

--- esb_app/src/esb/esb.c
+++ esb_app/src/esb/esb.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
 #include "email.h"
+#include "esb.h"
 
 /**
- * This is the main entry point into the ESB. 
- * It will start processing of a BMD received at the HTTP endpoint.
+ * TODO: This is to be implemented separately.
  */
-int process_esb_request(char* bmd_file_path) {
-    int status = 0; // OK == 0
-    printf("Handling the BMD %s\n", bmd_file_path);
-    /** TODO: 
-     * Perform the steps outlined in the Theory of Operation section of
-     * the ESB specs document. Each major step should be implemented in
-     * a separate module. Suitable unit tests should be created for all
-     * the modules, including this one.
-     */
-    return status;
+bmd parse_bmd_xml(char* bmd_file_path) {
+    bmd b;
+    bmd_envelop envl;
+    envl.sender_id = "TEST-GUID-1";
+    envl.destination_id = "TEST-GUID-2";
+    envl.message_type = "TEST-GUID-3";
+
+    b.envelop = envl;
+    b.payload = "Some data here";
+    return b;
 }
+
+int is_bmd_valid(bmd b)
+{
+    int valid = 1; // 1 => vaild, -1 => invalid
+    // TODO: Implement the validation logic here
+
+    return valid;
+}
+
