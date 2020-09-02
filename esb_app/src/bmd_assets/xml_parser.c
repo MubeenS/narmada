35910a5506889cb79b3400f762be1994f69b2ac2
MubeenS <sk.md.mubeen@gmail.com>
2020-09-02 12:42:40 +0530

    xml parser

--- esb_app/src/bmd_assets/xml_parser.c
+++ esb_app/src/bmd_assets/xml_parser.c
@@ -1,4 +1,34 @@
-#include 
-/** Contains envelope and payload structs*/
+/**
+ * @file xml_parser.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-02
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <string.h>
+
+/*xml handling library*/
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+/* Contains envelope and payload structs*/
 #include "bmd.h"
 
+envelope *envelope_data;
+payload *payload_data;
+
+envelope_data extract_envelop(char* bmd_xml) {
+    /** TODO: code to extract data from xml and store in structuers*/
+    /** TODO: see Real example and former xml_parser.c */
+}
+
+payload_data extract_payload(char* bmd_xml) {
+    /**extract payload details*/
+}
+
+int main () {
+    /** TODO: Call functions from here*/
+    /** TODO: write test cases*/
+}
