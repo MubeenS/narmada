0b04219d6e9392eee9c0e2b57245698af8af5641
MubeenS <sk.md.mubeen@gmail.com>
2020-09-07 14:50:41 +0530

    esb_unit_tests

--- esb_app/src/esb/test_esb.c
+++ esb_app/src/esb/test_esb.c
@@ -12,12 +12,7 @@
 
 #include "../db_access/connector.h"
 
-/**
- * If the name of a test function is "test_abc" then you should
- * define the setup and teardown functions by the names:
- * test_abc_setup and test_abc_tear_down respectively.
- * 
- */
+
 static void *
 queue_the_request_setup(const MunitParameter params[], void *user_data)
 { /* Creates bmd and returns */
@@ -40,23 +35,32 @@ static MunitResult
 test_queue_the_request(const MunitParameter params[], void *fixture)
 {
     bmd *b = (bmd *)fixture;
-    /**
-     * Perform the checking of logic here as needed.
-     * Typically, you will invoke the function under testing
-     * here by passing it suitable data from fixture. Then,
-     * use assertions to verify the expected results.
-     * In this example, we are just checking the value of a
-     * string which we were expecting to be available in the
-     * fixture itself. This test will fail when you change the
-     * string. You will need to recompile and re-run the tests
-     * to see the effect of any changes in data in this example.
-     */
+
     munit_assert(queue_the_request(b)==1);
+    return MUNIT_OK;
+}
+
+
+static MunitResult
+test_process_esb_request(const MunitParameter params[], void *fixture)
+{
+    bmd *b = (bmd *)fixture;
+
+    munit_assert(process_esb_request("bmd1.xml")==1);
     return MUNIT_OK;
 }
 
 /* Put all unit tests here. */
 MunitTest esb_tests[] = {
+     {
+        "/process_esb_request",   /* name */
+        test_process_esb_request,  /* test function */
+        NULL,    /* setup function for the test */
+        NULL,    /* tear_down */
+        MUNIT_TEST_OPTION_NONE, /* options */
+        NULL                    /* parameters */
+    },
+    
     {
         "/queue_the_request",   /* name */
         test_queue_the_request,  /* test function */
